ba0b872680693921c1c55ff4809af8e5ea231cec
THT154 <165685792+THT154@users.noreply.github.com>
2024-04-10 10:54:36 +0700

    Add files via upload

---
+++ GiuaKi/src/Thread1.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Thread1 {
+	public static void main(String[] args) {
+		// Tạo sinh viên
+		LocalDate dob = LocalDate.of(2005, 4, 15);
+//		Student student = new Student("1", "Trần Hữu Thắng", "Quảng Bình", dob);
+		List<Student> danhsach = new ArrayList<Student>();
+
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse("src\\\\Student.xml");
+			Element element = doc.getDocumentElement();
+			
+			NodeList ID = element.getElementsByTagName("ID");
+			NodeList name = element.getElementsByTagName("name");
+			NodeList address = element.getElementsByTagName("address");
+			NodeList age = element.getElementsByTagName("dateOfBirth");
+			for (int i = 0; i < name.getLength(); i++) {
+				Student student = new Student();
+				student.setID(ID.item(i).getTextContent());
+				student.setName(name.item(i).getTextContent());
+				student.setAddress(address.item(i).getTextContent());
+			    String ageContent = age.item(i).getTextContent();
+			    LocalDate dateOfBirth = LocalDate.parse(ageContent);
+				student.setDateOfBirth(dateOfBirth);
+				danhsach.add(student);
+
+			}
+			
+			
+
+		} catch (ParserConfigurationException | SAXException | IOException ex) {
+			ex.printStackTrace();
+		}
+
+		for (Student student : danhsach) {
+			ThreadMain thread = new ThreadMain(student);
+			thread.Thread2();
+
+			thread.Thread3();
+			
+			thread.writeStudentsToXML(danhsach, "src\\kq.xml");
+//			System.out.println(student);
+		}
+		
+		
+
+	}
+}
---
+++ GiuaKi/src/ThreadMain.java
@@ -0,0 +1,151 @@
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class ThreadMain extends Thread {
+	private Student student;
+	private int sumOfDigits;
+	private Period age;
+
+	public ThreadMain(Student student) {
+		this.student = student;
+	}
+
+	public void Thread2() {
+
+		LocalDate dob = student.getDateOfBirth();
+
+		LocalDate now = LocalDate.now();
+		age = Period.between(dob, now);
+
+		System.out.println("-----------------------------------");
+		System.out.println("Tính tuổi của " + student.getName());
+		System.out.println("Years: " + age.getYears());
+		System.out.println("Months: " + age.getMonths());
+		System.out.println("Days: " + age.getDays());
+
+		String encodedAge = encodeAge(age.getYears(), age.getMonths(), age.getDays());
+
+		System.out.println("Tuổi sau khi mã hóa " + student.getName() + ": " + encodedAge);
+
+	}
+
+	public String encodeAge(int years, int months, int days) {
+		return String.valueOf(years) + String.valueOf(months) + String.valueOf(days);
+	}
+
+	public void Thread3() {
+		// Lấy ngày sinh của sinh viên
+		LocalDate dob = student.getDateOfBirth();
+
+		// Tính tổng các chữ số trong ngày tháng năm sinh
+		sumOfDigits = calculateSumOfDigits(dob.toString().replace("-", ""));
+
+		// Kiểm tra xem tổng có phải số nguyên tố hay không
+		boolean isPrime = isPrime(sumOfDigits);
+
+		// In ra kết quả kiểm tra
+		System.out.println("-----------------------------------");
+		System.out.println("Tổng các chữ số trong ngày tháng năm sinh của " + student.getName() + ": " + sumOfDigits);
+		if (isPrime == true) {
+			System.out.println(sumOfDigits + " là một số nguyên tố");
+		} else {
+			System.out.println(sumOfDigits + " không phải là một số nguyên tố");
+
+		}
+
+	}
+
+	// Phương thức tính tổng các chữ số trong một chuỗi số
+	private int calculateSumOfDigits(String number) {
+		int sum = 0;
+		for (int i = 0; i < number.length(); i++) {
+			sum += Character.getNumericValue(number.charAt(i));
+		}
+		return sum;
+	}
+
+	// Phương thức kiểm tra xem một số có phải số nguyên tố hay không
+	private boolean isPrime(int number) {
+		if (number <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(number); i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void writeStudentsToXML(List<Student> students, String filename) {
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.newDocument();
+
+			// Tạo phần tử gốc <students>
+			Element rootElement = doc.createElement("students");
+			doc.appendChild(rootElement);
+
+			// Lặp qua từng sinh viên và ghi thông tin vào tệp XML
+			for (Student student : students) {
+				Element studentElement = doc.createElement("student");
+				rootElement.appendChild(studentElement);
+
+				Element yearElement = doc.createElement("Year");
+				yearElement.appendChild(doc.createTextNode(String.valueOf(age.getYears())));
+				studentElement.appendChild(yearElement);
+
+				Element monthElement = doc.createElement("Months");
+				monthElement.appendChild(doc.createTextNode(String.valueOf(age.getMonths())));
+				studentElement.appendChild(monthElement);
+
+				Element dayElement = doc.createElement("Days");
+				dayElement.appendChild(doc.createTextNode(String.valueOf(age.getDays())));
+				studentElement.appendChild(dayElement);
+
+				ThreadMain thread3 = new ThreadMain(student);
+				thread3.start();
+
+				Element sumElement = doc.createElement("sum");
+				sumElement.appendChild(doc.createTextNode(String.valueOf(sumOfDigits)));
+				studentElement.appendChild(sumElement);
+
+				Element isDigitElement = doc.createElement("isDigit");
+				isDigitElement.appendChild(doc.createTextNode(String.valueOf(isPrime(sumOfDigits))));
+				studentElement.appendChild(isDigitElement);
+			}
+
+			// Ghi dữ liệu vào tệp XML
+			TransformerFactory tf = TransformerFactory.newInstance();
+			Transformer transformer = tf.newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(filename);
+			transformer.transform(source, result);
+
+			System.out.println("Dữ liệu đã được ghi vào tệp XML thành công.");
+		} catch (ParserConfigurationException | TransformerException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
